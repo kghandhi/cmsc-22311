d9245b098d0854bde29fd814fe119cb99b550cd6
kghandhi <kghandhi@uchicago.edu>
2015-04-25 18:16:47 -0500

    parsing into tags

--- Lab1-Sokal/lab1.hs
+++ Lab1-Sokal/lab1.hs
@@ -1,12 +1,17 @@
 import Network.HTTP
+import Text.HTML.TagSoup
 
+getArticle :: String -> [Tag String]
+getArticle pg = parseTags pg
 
 -- String: url
 --
-getBody :: String -> IO String
-getBody url = simpleHTTP (getRequest url) >>= getResponseBody
+getHttpBody :: String -> IO String
+getHttpBody url = simpleHTTP (getRequest url) >>= getResponseBody
 
+--look for div id = "body" tag is div, attr is id value of attr is body
 main = do
   cont <- getContents
   let lns = lines cont
-  mapM_ (\x -> getBody x >>= print) lns
+  -- mapM_ print lns
+  mapM_ (\x -> getHttpBody x >>= print . show . getArticle) lns
