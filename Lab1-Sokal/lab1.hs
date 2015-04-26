4f6da652fde61db8b1b1f5de23425af1f3755d73
kghandhi <kghandhi@uchicago.edu>
2015-04-26 18:50:17 -0500

    didnt do much

--- Lab1-Sokal/lab1.hs
+++ Lab1-Sokal/lab1.hs
@@ -1,8 +1,14 @@
 import Data.Maybe
 import Data.List
+import Data.Map hiding (foldl)
 import Network.HTTP
 import Text.HTML.TagSoup
 import Control.Monad
+
+type PrimitiveModel = Map (String, String) [String]
+type ProcessModel = [(String, [(Int, Int)])]
+
+
 
 openURL :: String -> IO String
 openURL url = simpleHTTP (getRequest url) >>= getResponseBody
@@ -15,8 +21,19 @@ getBody :: [Tag String] -> [Tag String]
 getBody tags =
   takeWhile (~/= "<div id=back>") $ head $ sections (~== "<div id=body>") tags
 
+extractWords :: [Tag String] -> [String]
+extractWords tags = words $ innerText $ harvest $ getBody tags
+
+makePairs :: [String] -> [(String, String)]
+makePairs txt = zip txt (drop 1 txt)
+
+-- makePrim txt = fold helper txt Map ("","") []
+--   let
+--     helper (x:y:z:rest)  = if (x,y) in thing then thing[(x,y)].append(t) else thing[(x,y)] = [t]
+
+-- suck :: [String] -> (PrimitiveModel, ProcessModel) -> (PrimitiveModel, ProcessModel)
+-- suck txt (prim, process) =
 
 main = do
-  tags <- fmap parseTags $ openURL "http://muse.jhu.edu/journals/postmodern_culture/v024/24.1.marriott.html"
-  let body = innerText $ harvest $ getBody tags
-  writeFile "body.txt" body
+  tags <- fmap parseTags $ openURL "http://muse.jhu.edu/journals/postmodern_culture/v024/24.1.mickalites.html"
+  print $ extractWords tags
