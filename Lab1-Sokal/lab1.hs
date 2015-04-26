f83d9326bd2c50789ef27bfa022bb40a00e397ec
kghandhi <kghandhi@uchicago.edu>
2015-04-26 17:28:39 -0500

    actually gets a beautiful string next turn it into words and do markov stuff

--- Lab1-Sokal/lab1.hs
+++ Lab1-Sokal/lab1.hs
@@ -4,34 +4,19 @@ import Network.HTTP
 import Text.HTML.TagSoup
 import Control.Monad
 
--- getArticle :: String -> [Tag String]
--- getArticle pg = parseTags pg
-
--- String: url
---
 openURL :: String -> IO String
 openURL url = simpleHTTP (getRequest url) >>= getResponseBody
 
---look for div id = "body" tag is div, attr is id value of attr is body
--- main = do
---   cont <- getContents
---   let lns = lines cont
---   -- mapM_ print lns
---   mapM_ (\x -> getHttpBody x >>= print . show . getArticle) lns
+harvest :: [Tag String] -> [Tag String]
+harvest body =
+  foldl (++) [] (sections (~== "<p>") body)
 
--- harvest :: Tag String -> [Tag String]
--- harvest body =
-  --foldl (++) [] (sections (~== "<p>") [body])
+getBody :: [Tag String] -> [Tag String]
+getBody tags =
+  takeWhile (~/= "<div id=back>") $ head $ sections (~== "<div id=body>") tags
 
-getBody :: [Tag String] -> Tag String
-getBody tags = fromJust $ find (== TagText "<div id=\"body\">") tags
 
 main = do
   tags <- fmap parseTags $ openURL "http://muse.jhu.edu/journals/postmodern_culture/v024/24.1.marriott.html"
-  let body = fromTagText $ getBody tags
-        --innerText $ harvest $ getBody tags
+  let body = innerText $ harvest $ getBody tags
   writeFile "body.txt" body
-
--- main = do
---   src <- openURL "http://muse.jhu.edu/journals/postmodern_culture/v024/24.1.marriott.html"
---   writeFile "temp.html" src
