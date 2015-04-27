78077023ff34269f34913c6c16b9c8c07075e061
kghandhi <kghandhi@uchicago.edu>
2015-04-27 14:15:16 -0500

    needed to reverse it, this should work but need to write to a file nwo

--- Lab1-Sokal/lab1.hs
+++ Lab1-Sokal/lab1.hs
@@ -48,9 +48,9 @@ makeProcess freq = fst $ mapAccumWithKey mapper [] freq where
   mapper process (x,y) zs = ((y, L.map (\(f,z) -> (f, findIndex (y,z) freq)) zs):process, zs)
 
 suck :: [String] -> ProcessModel
-suck txt = makeProcess $ makeFreq $ makePrim txt
+suck txt = reverse $ makeProcess $ makeFreq $ makePrim txt
 
 main = do
   tags <- fmap parseTags $ openURL "http://muse.jhu.edu/journals/postmodern_culture/v024/24.1.mickalites.html"
   --L.map (\tup -> writeFile "sokal.model" (show tup)) $ suck $ extractWords tags
-  print . show $ suck $ extractWords tags
+  print . show $ extractWords tags
