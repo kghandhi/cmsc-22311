4d07270cbff6c9d781eff87147080f34f2783d1f
kghandhi <kghandhi@uchicago.edu>
2015-04-27 14:02:36 -0500

    maybe this is suck

--- Lab1-Sokal/lab1.hs
+++ Lab1-Sokal/lab1.hs
@@ -1,6 +1,9 @@
 import Data.Maybe
-import Data.List hiding (insert, empty)
+import Data.Ord
+import Data.List hiding (insert, empty, findIndex)
+import qualified Data.List as L (map)
 import Data.Map hiding (foldl)
+import qualified Data.Map as M (map)
 import Network.HTTP
 import Text.HTML.TagSoup
 import Control.Monad
@@ -33,20 +36,21 @@ makePrim txt = helper (empty :: PrimitiveModel) txt
     helper acc (x:y:z:rest) = if member (x,y) acc then helper (adjust (++ [z]) (x,y) acc) (y:z:rest)
                               else helper (insert (x, y) [z] acc) (y:z:rest)
 
+makeFreq :: PrimitiveModel -> Map (String, String) [(Int, String)]
+makeFreq prim = M.map frequency prim
 
+frequency :: Ord a => [a] -> [(Int, a)]
+frequency xs = sortBy (flip $ comparing fst) $ L.map (\x -> (length x, head x)) (group $ sort xs)
 
+makeProcess :: Map (String, String) [(Int, String)] -> ProcessModel
+makeProcess freq = fst $ mapAccumWithKey mapper [] freq where
+  --mapper :: ProcessModel -> (String, String) -> [(Int, String)] -> [(String, [(Int, Int)])]
+  mapper process (x,y) zs = ((y, L.map (\(f,z) -> (f, findIndex (y,z) freq)) zs):process, zs)
 
-    -- helper acc (x:y:z:rest) = if member (x,y) acc then adjust (++ [z]) (x,y) acc
-    --                           else insert (x, y) [z] acc
-
--- makeProcess :: PrimitiveModel -> ProcessModel
--- makeProcess prim =
---   let
---     countFreq ss = sort ss
-
--- suck :: [String] -> (PrimitiveModel, ProcessModel) -> (PrimitiveModel, ProcessModel)
--- suck txt (prim, process) =
+suck :: [String] -> ProcessModel
+suck txt = makeProcess $ makeFreq $ makePrim txt
 
 main = do
   tags <- fmap parseTags $ openURL "http://muse.jhu.edu/journals/postmodern_culture/v024/24.1.mickalites.html"
-  print $ extractWords tags
+  --L.map (\tup -> writeFile "sokal.model" (show tup)) $ suck $ extractWords tags
+  print . show $ suck $ extractWords tags
