fd855839ef974f144c25f6446b57b5a1131f7b23
kghandhi <kghandhi@uchicago.edu>
2015-05-17 15:02:24 -0500

    passes all of my test cases up until escaped characters

--- Globber/Globber.hs
+++ Globber/Globber.hs
@@ -1,8 +1,12 @@
 module Globber (matchGlob) where
+import Data.List (any, drop, map)
 
 type GlobPattern = String
 
 matchGlob :: GlobPattern -> String -> Bool
-matchGlob ('?':xs) (_:ys) = matchGlob xs ys
--- other cases
+matchGlob ('*':xs) ys = any (matchGlob xs) (map (\i -> drop i ys) [0..(length ys)])
+matchGlob ('?':xs) (y:ys) = y /= ' ' &&  matchGlob xs ys
+matchGlob ('\\':x:xs) (y:ys) = x == y && matchGlob xs ys
 matchGlob (x:xs) (y:ys) = x == y && matchGlob xs ys
+matchGlob [] [] = True
+matchGlob _ _ = False
--- Globber/TestGlobber.hs
+++ Globber/TestGlobber.hs
@@ -6,7 +6,6 @@ import Globber
 
 main :: IO ()
 main = hspec $ describe "Testing Globber" $ do
-
     describe "case: empty pattern" $ do
       it "matches empty string" $
         matchGlob "" "" `shouldBe` True
@@ -15,3 +14,37 @@ main = hspec $ describe "Testing Globber" $ do
     describe "case: question mark" $ do
       it "matches any single character" $
         matchGlob "?" "*" `shouldBe` True
+      it "matches any character within a longer string" $
+        matchGlob "?s" "zs" `shouldBe` True
+      it "matches characters within a longer string 2" $
+        matchGlob "x?s" "xzs" `shouldBe` True
+      it "doesn't match the empty string" $
+        matchGlob "?" " " `shouldBe` False
+    describe "case: star" $ do
+      it "matches any string" $
+        matchGlob "*" "xyz" `shouldBe` True
+      it "matches any string at end of longer string" $
+        matchGlob "ab*" "abXYZ" `shouldBe` True
+      it "matches any string within a longer string" $
+        matchGlob "ab*bc" "abXYZbc" `shouldBe` True
+      it "doesn't match when more patterns follow that dont match" $
+        matchGlob "ab*bc" "abXYZmn" `shouldBe` False
+      it "does match when more patterns follow that match" $
+        matchGlob "ab*bc" "abXYZASEWERbc" `shouldBe` True
+      it "does match with use of question mark case" $
+        matchGlob "ab*?d" "abXYZmd" `shouldBe` True
+      it "matches with multiple stars" $
+        matchGlob "***************************************" "aaaaaaaa" `shouldBe` True
+    describe "case: escape character" $ do
+      it "matches a single escaped character" $
+        matchGlob "\\a" "a" `shouldBe` True
+      it "matches a single escaped special character" $
+        matchGlob "\\*" "*" `shouldBe` True
+      it "matches a single escaped character at the end of a string" $
+        matchGlob "ab*\\*" "abXYZ*" `shouldBe` True
+      it "matches an escaped character within a string" $
+        matchGlob "ab?\\?mno" "abZ?mno" `shouldBe` True
+      it "doesn't match wrong escaped character" $
+        matchGlob "\\a\\b\\c\\e" "abcd" `shouldBe` False
+
+    -- describe "case: edge cases"
