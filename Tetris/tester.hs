7c9591884e572e601bc04bb1373819b007db544f
Kira Ghandhi <kghandhi@uchicago.edu>
2015-05-30 16:56:54 -0500

    made tester.hs compile

--- Tetris/tester.hs
+++ Tetris/tester.hs
@@ -3,17 +3,17 @@ module Main where
 import FRP.Helm
 import qualified FRP.Helm.Time as Time
 import qualified FRP.Helm.Text as T
-import FRP.Helm.Signal ((<~), (~), Signal)
+import FRP.Helm.Signal ((<~), (~~), Signal)
 import qualified FRP.Helm.Signal as Signal
 import qualified FRP.Helm.Window as Window
 import qualified FRP.Helm.Color as Color
 import qualified FRP.Helm.Mouse as Mouse
-import qualified FRP.Helm.Mouse as G
+import qualified FRP.Helm.Graphics as G
 
 import Data.List
 
-data Click = (Time.Time, (Int, Int))
-data State = (Time.Time, [Click])
+type Click = (Time.Time, (Int, Int))
+type State = (Time.Time, [Click])
 
 initState = (0, [])
 
@@ -24,31 +24,36 @@ tfade = 2 * Time.second
 pruneOld :: Time.Time -> [Click] -> [Click]
 pruneOld now clicks =
   case clicks of
-   [] -> []
-   (t, xy):clicks' -> if (now-t) > tfade then []
-                      else (t,xy):(pruneOld now clicks')
+    [] -> []
+    (t, xy):clicks' -> if (now-t) > tfade then []
+                       else (t,xy):(pruneOld now clicks')
 
-upstate :: Update -> State -> State
-upstate u (_, clicks) =
-  case u of
-   NewTime t -> (t, pruneOld t clicks)
-   NewClick (t, xy) -> (t, (t, xy):(pruneOld t clicks))
+upstate :: [Update] -> State -> State
+upstate us (now, clicks) =
+  case us of
+    [] ->  (now, clicks)
+    (u:us') ->
+      case u of
+        NewTime t -> (t, (pruneOld t clicks))
+        NewClick (t, xy) -> (t, (t, xy):(pruneOld t clicks))
 
 setAlpha :: Double -> Color.Color -> Color.Color
-setAlpha a c = Color.rgba rgb.red rgb.green rgb.blue a
-  where rgb = Color.toRgb c
+setAlpha a (Color r g b _) = Color.rgba r g b a
+
+toNum :: (Num c, Integral a) => a -> c
+toNum = fromInteger . toInteger
 
 view :: (Int, Int) -> State -> G.Element
-view (w, h) (now, clicks) = G.collage w h dots
+view (w, h) (now, clicks) = G.centeredCollage w h dots
   where
-    (fw, fh) = (fromIntegral w, fromIntegral h)
+    (fw, fh) = (toNum w, toNum h)
     (dx, dy) = (-fw/2, fh/2)
-    color a = setAlpha a Color.navy
+    color a = setAlpha a Color.white
     rad pct = 20 + 100 *pct
     circ pct = G.filled (color (1-pct))  (G.circle (rad pct))
     dots = map (\(t,(x,y)) ->
                  let pct = (now-t) / tfade in
-                  C.move (fromIntegral x + dx, fromIntegral (-y) + dy) $ circ pct)
+                  G.move (toNum x + dx, toNum (-y) + dy) $ circ pct)
            clicks
 
 
@@ -62,6 +67,7 @@ clicks :: Signal Click
 clicks = timestamp $ seq Mouse.clicks Mouse.position
 
 state :: Signal State
-state = Signal.foldp upstate initState (head $ Signal.combine [NewTime <~ time, NewClick <~ clicks])
+state = Signal.foldp upstate initState (Signal.combine [NewTime <~ time, NewClick <~ clicks])
 
-main = view <~ Window.dimensions ~~ state
+main :: IO ()
+main = run defaultConfig $ view <~ Window.dimensions ~~ state
