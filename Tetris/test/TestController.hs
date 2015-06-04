d6f01b371e06c86eebbccdf3a9273e9ee692d3d0
Kira Ghandhi <kghandhi@uchicago.edu>
2015-06-04 18:38:31 -0500

    passed all the tests

--- Tetris/test/TestController.hs
+++ Tetris/test/TestController.hs
@@ -1,57 +1,71 @@
 module Main (main) where
 
+import Data.Array
 import Test.Hspec
+
 
 import Controller
 import Tetris
 
 main :: IO ()
 main = hspec $ describe "Testing the control operations" $ do
-  describe "test addition" $ do
-    it "should have an identity" $ do
-      (0 + 1,1 + 0) `shouldBe` (1, 1)
+  describe "test isBarrier" $ do
+    it "should detect walls" $ do
+      let locs = [(1,1),(1,2),(1,3),(1,4)]
+      let lft = [((0,y), Wall) | y <- [0..4]]
+      let rgt = [((3,y), Wall) | y <- [0..4]]
+      let tet = [((1,y), Filled (I locs)) | y <- [1..4]]
+      let midd = [((2,y), Empty) | y <- [1..4]]
+      let btm = [((x,0), Wall) | x <- [1..2]]
+      let tnyBd = array ((0,0),(3,4)) (lft ++ rgt ++ btm ++ tet ++ midd)
+      isBarrier (1,1-1) locs tnyBd `shouldBe` True
+    it "should not be a barrier if its empty" $ do
+      let locs = [(1,2),(1,3),(1,4),(1,5)]
+      let lft = [((0,y), Wall) | y <- [0..5]]
+      let rgt = [((3,y), Wall) | y <- [0..5]]
+      let btm = [((x,0), Wall) | x <- [1..2]]
+      let tet = [((1,y), Filled (I locs)) | y <- [2..5]]
+      let midd = ((1,1), Empty):[((2,y), Empty) | y <- [1..5]]
+      let tnyBd = array ((0,0),(3,5)) (lft ++ rgt ++ btm ++ tet ++ midd)
+      isBarrier (1,2-1) locs tnyBd `shouldBe` False
   describe "Check hasLanded" $ do
     it "should be true when it lands on the floor" $ do
       let locs = [(1,1),(1,2),(1,3),(1,4)]
-          lft = [((0,y), Wall) | y <- [0..4]]
-          rgt = [((3,y), Wall) | y <- [0..4]]
-          tet = [((1,y), I locs) | y <- [1..4]]
-          midd = [((2,y), Empty) | y <- [1..4]]
-          btm = [((x,0), Wall) | x <- [1..2]]
-          tnyBd = array ((0,0),(3,4)) (lft ++ rgt ++ btm ++ tet ++ midd)
-        in
-       hasLanded locs tnyBd `shouldBe` True
+      let lft = [((0,y), Wall) | y <- [0..4]]
+      let rgt = [((3,y), Wall) | y <- [0..4]]
+      let tet = [((1,y), Filled (I locs)) | y <- [1..4]]
+      let midd = [((2,y), Empty) | y <- [1..4]]
+      let btm = [((x,0), Wall) | x <- [1..2]]
+      let tnyBd = array ((0,0),(3,4)) (lft ++ rgt ++ btm ++ tet ++ midd)
+      hasLanded locs tnyBd `shouldBe` True
     it "Shold be false when its a block above floor" $ do
-      let locs = [(1,1),(1,2),(1,3),(1,4)]
-          lft = [((0,y), Wall) | y <- [0..5]]
-          rgt = [((3,y), Wall) | y <- [0..5]]
-          btm = [((x,0), Wall) | x <- [1..2]]
-          tet = [((1,y), I locs) | y <- [2..5]]
-          midd = [((2,y), Empty) | y <- [1..5]]
-          tnyBd = array ((0,0),(3,5)) (lft ++ rgt ++ btm ++ tet ++ midd)
-        in
-       hasLanded locs tnyBd `shouldBe` False
+      let locs = [(1,5),(1,2),(1,3),(1,4)]
+      let lft = [((0,y), Wall) | y <- [0..5]]
+      let rgt = [((3,y), Wall) | y <- [0..5]]
+      let btm = [((x,0), Wall) | x <- [1..2]]
+      let tet = [((1,y), Filled (I locs)) | y <- [2..5]]
+      let midd = ((1,1),Empty):[((2,y), Empty) | y <- [1..5]]
+      let tnyBd = array ((0,0),(3,5)) (lft ++ rgt ++ btm ++ tet ++ midd)
+      hasLanded locs tnyBd `shouldBe` False
     it "Should be true when it lands on another one" $ do
       let sqr = O [(1,1),(2,2),(1,2),(2,1)]
-          me = [(1,3),(1,4),(1,5),(1,6)]
-          lft = [((0,y), Wall) | y <- [0..6]]
-          rgt = [((3,y), Wall) | y <- [0..6]]
-          tet = [((1,y), I locs) | y <- [3..6]]
-          friend = [((i,j), sqr) | i <- [1..2], j <- [1..2]]
-          midd = [((2,y), Empty) | y <- [3..6]]
-          btm = [((x,0), Wall) | x <- [1..2]]
-          tnyBd = array ((0,0),(3,6)) (lft ++ rgt ++ btm ++ tet ++ midd ++ friend)
-        in
-       hasLanded locs tnyBd `shouldBe` True
+      let locs = [(1,3),(1,4),(1,5),(1,6)]
+      let lft = [((0,y), Wall) | y <- [0..6]]
+      let rgt = [((3,y), Wall) | y <- [0..6]]
+      let tet = [((1,y), Filled (I locs)) | y <- [3..6]]
+      let friend = [((i,j), Filled sqr) | i <- [1..2], j <- [1..2]]
+      let midd = [((2,y), Empty) | y <- [3..6]]
+      let btm = [((x,0), Wall) | x <- [1..2]]
+      let tnyBd = array ((0,0),(3,6)) (lft ++ rgt ++ btm ++ tet ++ midd ++ friend)
+      hasLanded locs tnyBd `shouldBe` True
     it "Can land in the sense that it is hanging" $ do
       let line = I [(1,1),(1,2),(1,3),(1,4)]
-          me = [(1,5),(2,5),(2,4),(2,3)]
-          lft = [((0,y), Wall) | y <- [0..5]]
-          rgt = [((3,y), Wall) | y <- [0..5]]
-          tet = [(xy, J locs) | xy <- locs]
-          friend [((1,y), line) | y <- [1..4]]
-          midd = [((2,y), Empty) | y <- [1..2]]
-          btm = [((x,0), Wall) | x <- [1..2]]
-          tnyBd = array ((0,0),(3,5)) (lft ++ rgt ++ btm ++ tet ++ midd ++ friend)
-        in
-       hasLanded me `shouldBe` True
+      let me = [(1,5),(2,5),(2,4),(2,3)]
+      let lft = [((0,y), Wall) | y <- [0..5]]
+      let rgt = [((3,y), Wall) | y <- [0..5]]
+      let tet = [(xy, Filled (J me)) | xy <- me]
+      let friend = [((1,y), Filled line) | y <- [1..4]]
+      let midd = [((2,y), Empty) | y <- [1..2]]
+      let btm = [((x,0), Wall) | x <- [1..2]]
+      let tnyBd = array ((0,0),(3,5)) (lft ++ rgt ++ btm ++ tet ++ midd ++ friend)
+      hasLanded me tnyBd `shouldBe` True
