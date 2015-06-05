8bcab3938402a397a85a341715a8cb201f208037
kghandhi <kghandhi@uchicago.edu>
2015-06-05 13:16:24 -0500

    hopefully i wrote a main

--- Tetris/Main.hs
+++ Tetris/Main.hs
@@ -1,4 +1,33 @@
 module Main where
 
+import FRP.Helm
+import qualified FRP.Helm.Window as Window
+import FRP.Helm.Time
+import FRP.Helm.Signal
+import FRP.Helm.Keyboard
+
+import Controller
+import View
+
+merge :: Signal a -> Signal a -> Signal a
+merge s1 s2 =
+  let
+    tsMerge (t1,v1) (t2,v2)
+      | t1 >= t2 = v1
+      | otherwise = v2
+  in
+   tsMerge <~ timestamp s1 ~~ timestamp s2
+
+
+(Time -> b -> b) -> b -> Signal Time -> singal b
+actions :: Signal Action
+actions = merge (lift (\ks -> KeyAction (head ks)) keysDown)
+          (foldp (\_ -> TimeAction) (TimeAction) (fps 40))
+
+currState :: Signal State
+currState = foldp upstate initState actions
+
 main :: IO ()
-main = return ()
+main = do
+  engine <- startup defaultConfig
+  run engine $ view <~ Window.dimensions ~~ currState
