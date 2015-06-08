b6304a8683f4e08f4455d6878f049ab43f77eb71
Kira Ghandhi <kghandhi@uchicago.edu>
2015-06-08 01:31:58 -0500

    works on two computers, looks ugly will clean

--- Tetris/Main.hs
+++ Tetris/Main.hs
@@ -24,7 +24,7 @@ mergy st sk = lift2 (\t ks -> if ks == [] then TimeAction
                               else KeyAction (head ks)) st sk
 
 mergyAction :: Signal Action
-mergyAction = mergy (fps 2.4) (keysDown)
+mergyAction = mergy (fps 1.7) (keysDown)
 
 hackHead :: [Key] -> Key
 hackHead [] = TabKey
@@ -32,7 +32,7 @@ hackHead ks = head ks
 
 --(Time -> b -> b) -> b -> Signal Time -> singal b
 actions :: Signal Action
-actions = (merge (foldp (\_ _-> TimeAction) (TimeAction) (fps 2.4))
+actions = (merge (foldp (\_ _-> TimeAction) (TimeAction) (fps 1.7))
           (lift (\ks -> KeyAction (hackHead ks)) keysDown))
 
 currState :: Signal State
